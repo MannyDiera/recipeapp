0d817f1edf598d917fc3146a9bf916927f354573
= <=>
2018-12-17 21:56:23 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Recipe></Recipe>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Recipe from '@/components/Recipe.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, Recipe
   }
 }
 </script>
